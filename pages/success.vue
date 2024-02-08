68b5440a59ff75af31d7ac0fe75ba55c07328347
Estelle Rippe <rippestelle@gmail.com>
2024-02-08 20:05:20 +0100

    success page

---
+++ pages/success.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container">
+    <h1>Votre message a bien été envoyé</h1>
+    <p>
+      Merci d'avoir pris contact avec mon entreprise. Je vous recontacte dans
+      les plus brefs délais.
+    </p>
+    <p>
+      Si vous avez rencontré une erreur, n'hésitez pas à me contacter par mail à
+      l'adresse estelle.rippe.pro@gmail.com
+    </p>
+  </section>
+</template>
