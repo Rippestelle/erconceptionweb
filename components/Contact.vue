d1e0187cf9d86c948a4f814e6c52ebd655b61761
Estelle Rippe <rippestelle@gmail.com>
2024-02-09 13:34:32 +0100

    contact form

--- components/Contact.vue
+++ components/Contact.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script setup lang="js">
-import ContactForm from "../public/ContactForm.html";
+import ContactForm from "../public/ContactForm.vue";
 
 onMounted(() => {
   moveElements({
